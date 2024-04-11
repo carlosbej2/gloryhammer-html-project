86cd5033a739c4e7be68b79541918ff30d8e47f3
Carlos Bayón Guerrero <carlosupv86@gmail.com>
2024-04-11 20:52:39 +0200

    [ADD] Añadimos el script a todas las vistas y añadimos la animación del logo

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,14 @@ function startRotation(element) {
 function stopRotation(element) {
     element.style.transform = ''; // Reinicia la rotación
 }
+
+function startLogoAnimation(element) {
+    element.style.transition = 'transform 0.5s ease-in-out'; // Establece una transición suave para la animación
+    element.style.transform = 'scale(1.1)'; // Aplica un efecto de escala cuando el cursor está sobre la imagen
+}
+
+function stopLogoAnimation(element) {
+    element.style.transition = 'transform 0.5s ease-in-out'; // Establece una transición suave para la animación
+    element.style.transform = 'scale(1)'; // Devuelve la imagen a su tamaño original cuando el cursor se retira
+}
+
