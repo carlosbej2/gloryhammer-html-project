54739b997d20bf2573709ec954411592df81a4a6
Carlos Bayón Guerrero <carlosupv86@gmail.com>
2024-04-11 23:25:32 +0200

    [ADD] Añadimos vistas de discos, más funcionalidades en Javascript y estilos

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,12 @@ function stopLogoAnimation(element) {
     element.style.transition = 'transform 0.5s ease-in-out'; // Establece una transición suave para la animación
     element.style.transform = 'scale(1)'; // Devuelve la imagen a su tamaño original cuando el cursor se retira
 }
+
+function makeBigger(introText) {
+    introText.style.backgroundColor = 'rgba(0, 0, 0, .5)'; //Oscurecer el fondo al pasar el cursor sobre él
+    introText.style.borderRadius = '5%'; // Hace que el borde del texto sea redondeado
+}
+
+function makeDwarf(introText) {
+    introText.style.backgroundColor = 'transparent'; // Devuelve el fondo a su color original cuando el cursor se quita de encima
+}
